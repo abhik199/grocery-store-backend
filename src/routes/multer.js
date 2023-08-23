6c6b2f2d32b72c25cbec06ee87bb3ba2a34f845c
Abhishek <115573643+abhik199@users.noreply.github.com>
2023-08-24 01:30:38 +0530

    Update multer.js

--- src/routes/multer.js
+++ src/routes/multer.js
@@ -1,12 +1,15 @@
 const multer = require("multer");
 const path = require("path");
+const fs = require("fs");
 
 const fileStorage = multer.diskStorage({
   destination: (req, file, cb) => {
+    let uploadPath = ""; // Define the upload path
+    
     if (file.fieldname === "product_images") {
       uploadPath = path.join(__dirname, "../public/product");
     } else if (file.fieldname === "profile") {
-      (cb, "../../public/profile");
+      uploadPath = path.join(__dirname, "../../public/profile");
     } else if (file.fieldname === "category_image") {
       uploadPath = path.join(__dirname, "../public/category");
     } else if (file.fieldname === "subcategory_image") {
@@ -15,6 +18,15 @@ const fileStorage = multer.diskStorage({
       console.log(`multer problem ${file.fieldname}`);
       return cb(new Error("Invalid fieldname"));
     }
+
+    // Use fs module to create the folder
+    fs.mkdir(uploadPath, { recursive: true }, (err) => {
+      if (err) {
+        console.error('Error creating folder:', err);
+        return cb(err);
+      }
+      cb(null, uploadPath);
+    });
   },
   filename: (req, file, cb) => {
     cb(
