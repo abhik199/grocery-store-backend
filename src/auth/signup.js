4895f316c98bc177a3587d8980b823e5b5f483f1
Abhishek <115573643+abhik199@users.noreply.github.com>
2023-09-25 18:13:07 +0530

    Update signup.js

--- src/auth/signup.js
+++ src/auth/signup.js
@@ -37,7 +37,7 @@ exports.userRegistration = async (req, res, next) => {
         });
       }
      return res.status(400).json({
-  status: false,
+  status: true,
   statusCode: 400,
   message: 'Email is already taken',
 });
