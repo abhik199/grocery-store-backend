603b46bf7f1012a831e3abbd28e1ee660fdd0c03
Abhishek <115573643+abhik199@users.noreply.github.com>
2023-10-05 12:14:17 +0530

    Update order.js

--- src/controllers/users/order.js
+++ src/controllers/users/order.js
@@ -168,6 +168,10 @@ exports.fetchAllOrderByUse = async (req, res, next) => {
   const page = Number(req.query.page) || 1;
   const limit = Number(req.query.limit) || 5;
   const offset = (page - 1) * limit;
+  
+  const totalCount = await orderModel.count({
+    where: { userId: id }
+  })
 
   try {
     const Order = await orderModel.findAll({
@@ -180,7 +184,7 @@ exports.fetchAllOrderByUse = async (req, res, next) => {
     if (Order.length === 0) {
       return res.status(404).json({ status: false, message: "No order found" });
     }
-    const totalCount = Order.length;
+  
     const totalPages = Math.ceil(totalCount / limit);
     
     return res.status(200).json({ 
